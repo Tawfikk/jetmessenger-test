ababc38f0e06e7f5ce98f709088bab12a81161f0
Abdullah Tawfik <abdultawfikk@gmail.com>
2019-10-28 19:32:54 +0300

    first commit

---
+++ jetmessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jetmessenger
+//
+//  Created by Abdul Tawfik on 28.10.2019.
+//  Copyright © 2019 tawfik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
